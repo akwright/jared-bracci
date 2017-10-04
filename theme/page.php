d3967c8bd640f4df9d6687d46b7c9cc36c45dd1c
akwright <alexander.k.wright@gmail.com>
2017-10-03 23:08:28 -0400

    Added templates for child pages.

---
+++ theme/page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<section class="split  split--left">
+    <figure class="split-bgimage">
+      <img src="<?php echo get_field( 'page_background_image' ); ?>" alt="Left split background">
+    </figure>
+  </section>
+
+  <section class="split  split--right">
+    <h1 class="page-title">
+      <?php the_title(); ?>
+    </h1>
+
+    <?php
+      if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+      endwhile;
+      else :
+        _e( 'Sorry, no posts matched your criteria.', 'jaredbracci' );
+      endif;
+    ?>
+  </section>
+
+<?php get_footer(); ?>
