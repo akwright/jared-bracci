d3d585bde5d7893217ea127ea079b81bbd3acb4e
akwright <alexander.k.wright@gmail.com>
2018-02-04 20:59:52 -0500

    Added and styled footer. Fixed clipping of overflow for desktop. Styled mobile page titles.

--- theme/footer.php
+++ theme/footer.php
@@ -9,7 +9,14 @@
 </main>
 <?php // common footer content goes here ?>
 
-
+<footer class="site-footer">
+  <p class="copyright">&copy; <?php echo date("Y"); ?> Jared Bracci</p>
+  <ul class="footer-links">
+    <li><a href="https://itunes.apple.com/us/artist/jared-bracci/1083049454" target="_blank">iTunes</a></li>
+    <li><a href="https://open.spotify.com/artist/2efW1CdEX7c9CcyuccAnpa" target="_blank">Spotify</a></li>
+    <li><a href="https://www.facebook.com/JaredBracciMusic/" target="_blank">Facebook</a></li>
+  </ul>
+</footer>
 
 <?php wp_footer(); ?>
 
