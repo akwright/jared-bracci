ee56d41cbee57a4f133a2d4f5acaaf90ec5964cb
akwright <alexander.k.wright@gmail.com>
2018-06-05 21:16:45 -0400

    Added style for 404 page. Updated the footer to not be indented on 404 page.

--- theme/404.php
+++ theme/404.php
@@ -6,8 +6,18 @@
 ?>
 <?php get_header(); ?>
 
-<h1>
-	404 Page Not Found
-</h1>
+<article class="hero" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/jared-404.jpg');">
+  <div class="hero-content">
+
+    <header>
+      <h1 class="hero-title">
+        Oh no! That doesn't exist!
+      </h1>
+    </header>
+
+    <p>I'm not sure how you got here, but I'm sure this isn't where you meant to go. Why don't you <a href="/music/">check out some of my music</a> or head on back to <a href="/">the homepage</a>.</p>
+
+  </div>
+</article>
 
 <?php get_footer(); ?>
